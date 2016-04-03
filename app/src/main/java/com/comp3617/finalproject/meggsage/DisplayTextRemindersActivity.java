48af6967881dd77b12545b028705cbbfd7d75446
meggz <pixelbunnyoverlord@gmail.com>
2016-04-03 14:21:50 -0700

    initial commit

---
+++ app/src/main/java/com/comp3617/finalproject/meggsage/DisplayTextRemindersActivity.java
@@ -0,0 +1,12 @@
+package com.comp3617.finalproject.meggsage;
+
+import android.os.Bundle;
+
+public class DisplayTextRemindersActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_text_reminders);
+    }
+}
